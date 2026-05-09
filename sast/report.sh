89e95f6e74e1823c43c460caa0047626e8a6922e
madmti <supermatrix9@gmail.com>
2026-05-09 18:55:54 -0400

    feat: sast using semgrep

---
+++ sast/report.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+REPORTS_DIR="../reports"
+
+show_sast_reports() {
+    echo -e "${YELLOW}--- CATEGORÍA: SAST (Semgrep) ---${NC}"
+
+    files=("$REPORTS_DIR"/*.sast-fast.json)
+
+    if [ ! -e "${files[0]}" ]; then
+        echo -e "No hay reportes de SAST encontrados.\n"
+        return
+    fi
+
+    for report in "${files[@]}"; do
+        repo_name=$(basename "$report" .sast-fast.json)
+        echo -e "Repo: $repo_name"
+
+        jq -c '.results[]' "$report" | while read -r result; do
+            message=$(echo "$result" | jq -r '.extra.message')
+            file_path=$(echo "$result" | jq -r '.path')
+
+            # Extraemos inicio y fin
+            start_line=$(echo "$result" | jq -r '.start.line')
+            end_line=$(echo "$result" | jq -r '.end.line')
+
+            if [ -f "$file_path" ]; then
+                code_block=$(sed -n "${start_line},${end_line}p" "$file_path")
+            else
+                code_block="[No se pudo leer el archivo]"
+            fi
+
+            echo -e "[\u001b[31m!\u001b[0m] $message"
+            echo -e " => leak on \u001b[1;33m$file_path\u001b[0m at \u001b[1;33mlines $start_line-$end_line\u001b[0m"
+            echo -e " => detected:"
+            echo -e "\u001b[31m$code_block\u001b[0m\n"
+        done
+    done
+}
+
+echo "========================================"
+echo "SAST Scanning Reports (Semgrep)"
+echo "========================================"
+show_sast_reports
---
+++ sast/scan.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+REPORTS_DIR="../reports"
+mkdir -p "$REPORTS_DIR"
+
+declare -A REPO_CONFIGS
+REPO_CONFIGS["juice-shop"]="p/owasp-top-ten p/typescript p/javascript"
+REPO_CONFIGS["pygoat"]="p/owasp-top-ten p/python p/django"
+REPO_CONFIGS["vulhub"]="p/owasp-top-ten p/docker p/docker-compose"
+
+DEFAULT_CONFIG="p/security-audit"
+
+scan() {
+    local repo_name=$1
+    local configs=$2
+    local target_path=$3
+    local report_path="$REPORTS_DIR/${repo_name}.sast-fast.json"
+
+    read -ra CONFIG_ARRAY <<< "$configs"
+
+    local SEMGREP_ARGS=()
+    for cfg in "${CONFIG_ARRAY[@]}"; do
+        SEMGREP_ARGS+=("--config" "$cfg")
+    done
+    # -------------------------------------
+
+    printf "[SAST SCAN] $repo_name : "
+
+    semgrep scan "${SEMGREP_ARGS[@]}" --json -o "$report_path" "$target_path" >/dev/null 2>&1
+
+    RES=$?
+
+    if [ $RES -eq 0 ]; then
+        if [ -s "$report_path" ] && [ "$(jq '.results | length' "$report_path")" -gt 0 ]; then
+            echo -e "${RED}Found vulnerabilities${NC}\t $report_path"
+        else
+            echo -e "${GREEN}No issues found${NC}"
+            rm -f "$report_path"
+        fi
+    else
+         echo -e "${RED}Scan failed or issues found${NC}"
+    fi
+}
+
+echo "========================================"
+echo "SAST Scanning Tests (Semgrep)"
+echo "========================================"
+
+for dir in ../test-repos/*/; do
+    [ -e "$dir" ] || continue
+    repo=$(basename "$dir")
+
+    if [[ -v REPO_CONFIGS[$repo] ]]; then
+        current_configs=${REPO_CONFIGS[$repo]}
+    else
+        current_configs=$DEFAULT_CONFIG
+    fi
+
+    scan "$repo" "$current_configs" "$dir"
+done
